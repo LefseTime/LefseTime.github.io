07974eec45b3ee93c51a119f179132e5e3357ad2
LefseTime <36971616+LefseTime@users.noreply.github.com>
2018-09-15 18:20:15 -0500

    added individual projects

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Card = props => (
-    
-            <div className="card">
-                <h1>{props.title}</h1>
-                <p>{props.text}</p>
-                <a href={props.deplink} target="_blank" rel="noopener noreferrer" className="cardlink" id="view"><i className="fas fa-binoculars fa-2x"></i></a>
-                <a href={props.ghlink} target="_blank" rel="noopener noreferrer" className="cardlink" id="github"><i className="fab fa-github fa-2x"></i></a>
-            </div>
+
+    <div className="card">
+        <h1>{props.title}</h1>
+        <p>{props.text}</p>
+        <a href={props.deplink} target="_blank" rel="noopener noreferrer" className="cardlink" id="view"><i className="fas fa-binoculars fa-2x"></i></a>
+        <a href={props.ghlink} target="_blank" rel="noopener noreferrer" className="cardlink" id="github"><i className="fab fa-github fa-2x"></i></a>
+    </div>
 )
 
 export default Card;
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -4,7 +4,28 @@ import Card from "../../components/Card";
 class Portfolio extends Component {
 
     render() {
-        const cardProps = [
+        const soloCardProps = [
+            {
+                title: 'DEM BURGS',
+                text: 'A full-stack app using a MySQL database to display burgs available to nom, and allows users to nom them or add their own.',
+                deplink: 'https://dem-burgs.herokuapp.com/',
+                ghlink: 'https://github.com/LefseTime/DEM-BURGS'
+            },
+            {
+                title: 'Glorious Cheese Shoppe',
+                text: 'A Node/MySQL app with customer and manager views for the Glorious Cheese Shoppe allowing each various capabilities for viewing, buying, and stocking various Glorious Cheeses of the World.',
+                // deplink: 'https://github.com/LefseTime/Glorious-Cheese-Shoppe',
+                ghlink: 'https://github.com/LefseTime/Glorious-Cheese-Shoppe'
+            },
+            {
+                title: 'Kitty Klick',
+                text: 'A Node/MySQL app with customer and manager views for the Glorious Cheese Shoppe allowing each various capabilities for viewing, buying, and stocking various Glorious Cheeses of the World.',
+                deplink: 'https://lefsetime.github.io/kitty-klick/',
+                ghlink: 'https://github.com/LefseTime/Kitty-Klick'
+            }
+        ];
+
+        const groupCardProps = [
             {
                 title: 'Pet Cupid',
                 text: 'Find your perfect fluffy friend!',
@@ -24,18 +45,17 @@ class Portfolio extends Component {
                 <div className="scroll">
                     <h1 className="title">Portfolio</h1>
                     <hr></hr>
-                    <h2 className="title center">Group Projects</h2>
-                    <div className="cards">
-                        <Card {...cardProps[0]} />
-                        <Card {...cardProps[1]} />
-                        <Card />
-                    </div>
-                    <hr></hr>
                     <h2 className="title center">Individual Work</h2>
                     <div className="cards">
-                        <Card />
-                        <Card />
-                        <Card />
+                        <Card {...soloCardProps[0]} />
+                        <Card {...soloCardProps[1]} />
+                        <Card {...soloCardProps[2]} />
+                    </div>
+                    <hr></hr>
+                    <h2 className="title center">Group Projects</h2>
+                    <div className="cards">
+                        <Card {...groupCardProps[0]} />
+                        <Card {...groupCardProps[1]} />
                     </div>
                 </div>
             </div>
