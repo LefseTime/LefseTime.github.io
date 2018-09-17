8ab7279e07de57d2c2b153e584c0dbee190e1412
LefseTime <36971616+LefseTime@users.noreply.github.com>
2018-09-17 13:04:46 -0500

    card formatting

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 const Card = props => (
-
     <div className="card">
-        <h1>{props.title}</h1>
-        <p>{props.text}</p>
-        <a href={props.deplink} target="_blank" rel="noopener noreferrer" className="cardlink" id="view"><i className="fas fa-binoculars fa-2x"></i></a>
-        <a href={props.ghlink} target="_blank" rel="noopener noreferrer" className="cardlink" id="github"><i className="fab fa-github fa-2x"></i></a>
+        <div className="cardhover">
+            <div className="cardtext">
+                <h1>{props.title}</h1>
+                <p>{props.text}</p>
+                <a href={props.deplink} target="_blank" rel="noopener noreferrer" className="cardlink" id="view" className="whitelink"><i className="fas fa-binoculars fa-2x"></i></a>
+                <a href={props.ghlink} target="_blank" rel="noopener noreferrer" className="cardlink" id="github" className="whitelink"><i className="fab fa-github fa-2x"></i></a>
+            </div>
+            {/* <div className="cardlinks">
+                <a href={props.deplink} target="_blank" rel="noopener noreferrer" className="cardlink" id="view" className="whitelink"><i className="fas fa-binoculars fa-2x"></i></a>
+                <a href={props.ghlink} target="_blank" rel="noopener noreferrer" className="cardlink" id="github" className="whitelink"><i className="fab fa-github fa-2x"></i></a>
+            </div> */}
+        </div>
     </div>
 )
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -27,9 +27,9 @@ class Footer extends Component {
   render() {
     return (
       <div className="footer" id="footer">
-        <a href="https://github.com/LefseTime" target="_blank" rel="noopener noreferrer" id="github"><i className="fab fa-github fa-2x"></i></a>
-        <a href="https://www.linkedin.com/in/scfunkenbusch/" target="_blank" rel="noopener noreferrer" id="linkedin"><i className="fab fa-linkedin fa-2x"></i></a>
-        <a href="mailto:scfunkenbusch@gmail.com" id="email"><i className="fas fa-envelope fa-2x"></i></a>
+        <a href="https://github.com/LefseTime" target="_blank" rel="noopener noreferrer" id="github" className="blacklink"><i className="fab fa-github fa-2x"></i></a>
+        <a href="https://www.linkedin.com/in/scfunkenbusch/" target="_blank" rel="noopener noreferrer" id="linkedin" className="blacklink"><i className="fab fa-linkedin fa-2x"></i></a>
+        <a href="mailto:scfunkenbusch@gmail.com" id="email" className="blacklink"><i className="fas fa-envelope fa-2x"></i></a>
       </div>
     );
   }
