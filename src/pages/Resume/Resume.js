714aadcfb62b7a186c687f014b2905418b5d7495
LefseTime <36971616+LefseTime@users.noreply.github.com>
2018-09-24 17:40:16 -0500

    resume image and download

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import pdf from '../../pdf/resume.pdf'
 
 class Resume extends Component {
   state = {
@@ -29,8 +30,12 @@ class Resume extends Component {
         <div className="scroll">
           <h1 className="title">Resume</h1>
           <hr></hr>
+          <a href={pdf} download="FunkenbuschResume" className="whitelink" id="resumeDL">Download Resume</a>
+          <img src="https://i.imgur.com/ngogRVn.jpg" title="Resume Page 1" className="resume" id="resume1" />
+          <img src="https://i.imgur.com/J3uv4UF.jpg" title="Resume Page 2" className="resume" id="resume2" />
         </div>
       </div>
+
     );
   }
 }
