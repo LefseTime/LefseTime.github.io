a510073a3c5e8dde4c66152f6aa618f1ff43c3c2
LefseTime <36971616+LefseTime@users.noreply.github.com>
2018-10-11 18:55:24 -0500

    formatting changes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,10 +4,12 @@ class Card extends Component {
     
     render() {
 
+
+
         return (
             <div className="card" style={{backgroundImage: `url("${this.props.image}")`}}>
-                <div className="cardhover">
-                    <div className="cardtext">
+                <div className="cardhover" onclick="void(0)">
+                    <div className="cardtext" onclick="void(0)">
                         <h1>{this.props.title}</h1>
                         <p>{this.props.text}</p>
                         <a href={this.props.deplink} target="_blank" rel="noopener noreferrer" className="cardlink whitelink" id="view"><i className="fas fa-binoculars fa-2x"></i></a>
