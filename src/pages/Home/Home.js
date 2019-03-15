664084c327e24a36036b3ccd177e3487bb762630
LefseTime <scfunkenbusch@gmail.com>
2019-03-15 10:50:08 -0500

    one more link fixed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,7 +13,7 @@ class Home extends Component {
                         <p>Websites built. Databases managed. <a href="https://www.youtube.com/watch?v=TnZhi5gaX8g" target="_blank" rel="noopener noreferrer" id="catMassage">Cats massaged.</a></p>
                     </div>
                     <div className="buttons">
-                        <Button link="/portfolio" title="Portfolio" text="Portfolio" />
+                        <Button link="/portfolio-page" title="Portfolio" text="Portfolio" />
                         <Button link="/resume" title="Resume" text="Resume" />
                         <Button link="/about" title="About" text="About" />
                     </div>
